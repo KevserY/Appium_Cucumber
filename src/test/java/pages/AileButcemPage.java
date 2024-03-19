fe859c61c83321872b769f9dc09e29417be7d2dd
Kevser Yazici <mailkevseryazici@gmail.com>
2024-03-19 23:57:11 +0300

    Appium_Cucumber/6

--- src/test/java/pages/AileButcemPage.java
+++ src/test/java/pages/AileButcemPage.java
@@ -2,6 +2,7 @@ package pages;
 
 import io.appium.java_client.MobileElement;
 import io.appium.java_client.pagefactory.AppiumFieldDecorator;
+import org.junit.Assert;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 import utils.Driver;
@@ -26,6 +27,57 @@ public class AileButcemPage {
     @FindBy(xpath = "//*[@text='Giriş Yap']")
     public MobileElement girisYapButon;
 
+    @FindBy(xpath = "//*[@text='Başarıyla giriş yapıldı.']")
+    public MobileElement girisYapildiText;
+
+    @FindBy(xpath = "(//*[@class='android.widget.EditText'])[1]")
+    public MobileElement isimKutusu;
+
+    @FindBy(xpath = "(//*[@class='android.widget.EditText'])[2]")
+    public MobileElement soyisimKutusu;
+
+    @FindBy(xpath = "(//*[@class='android.widget.EditText'])[3]")
+    public MobileElement sehirkutusu;
+
+    @FindBy(xpath = "(//*[@class='android.widget.EditText'])[4]")
+    public MobileElement yaskutusu;
+
+    @FindBy(xpath = "(//*[@class='android.widget.EditText'])[5]")
+    public MobileElement meslekKutusu;
+
+
+    public void bilgiSilme(){
+        isimKutusu.clear();
+        soyisimKutusu.clear();
+        sehirkutusu.clear();
+        yaskutusu.clear();
+        meslekKutusu.clear();
+    }
+
+    public void textBoxVeriGirme(String isim,String soyisim,String sehir,String yas,String meslek){
+        bilgiSilme();
+        isimKutusu.sendKeys(isim);
+        soyisimKutusu.sendKeys(soyisim);
+        sehirkutusu.sendKeys(sehir);
+        yaskutusu.sendKeys(yas);
+        meslekKutusu.sendKeys(meslek);
+
+        ReusableMethods.scrollWithUiScrollable("Kaydet");
+    }
+
+    public void textBoxKontrol(String isim,String soyisim,String sehir,String yas,String meslek){
+        Assert.assertTrue(isimKutusu.getText().contains(isim));
+        Assert.assertTrue(soyisimKutusu.getText().contains(soyisim));
+        Assert.assertTrue(sehirkutusu.getText().contains(sehir));
+        Assert.assertTrue(yaskutusu.getText().contains(yas));
+        Assert.assertTrue(meslekKutusu.getText().contains(meslek));
+    }
+
+
+    public void ucCizgiTiklamaMethodu() throws InterruptedException {
+        ReusableMethods.wait(5);
+        ReusableMethods.koordinatTiklamaMethodu(107,130,500);
+    }
 
 
     public void ilkEkranOrtakAdimlari(int for1,int forSart,int x1,int y1,int wait,int x2,int y2,int bekleme,String girisText) throws InterruptedException {
@@ -37,15 +89,14 @@ public class AileButcemPage {
     }
 
 
-
     public void ilkEkranAyarlamalari() throws InterruptedException {
-
         for (int i = 0; i < 6; i++) {
             ReusableMethods.scrollScreenMethod(880,1400,750,150,1400,500);
         }
         ReusableMethods.scrollWithUiScrollable("Giriş Yap");
-
     }
+
+
 
 
 }
--- src/test/java/stepdefinitions/aileButcem.java
+++ src/test/java/stepdefinitions/aileButcem.java
@@ -5,6 +5,7 @@ import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.android.AndroidElement;
 import io.appium.java_client.touch.offset.PointOption;
 import io.cucumber.java.en.Given;
+import org.junit.Assert;
 import pages.AileButcemPage;
 import utils.ConfigReader;
 import utils.Driver;
@@ -26,37 +27,32 @@ public class aileButcem {
         aileButcemPage.ilkEkranOrtakAdimlari(for1,forSart,x1,y1,wait,x2,y2,bekleme,girisText);
     }
 
-    @Given("{string} ve {string} bilgilerini girerek kullanici bilgileriyle giris yapin")
-    public void mail_ve_password_bilgilerini_girerek_kullanici_bilgileriyle_giris_yapin(String mail,String password) {
+    @Given("{string} ve {string} bilgilerini girerek kullanici bilgileriyle {string} giris yapin")
+    public void mail_ve_password_bilgilerini_girerek_kullanici_bilgileriyle_giris_yapin(String mail,String password,String girisYap) {
         aileButcemPage.emailBox.sendKeys(ConfigReader.getProperty(mail));
         aileButcemPage.passwordBox.sendKeys(ConfigReader.getProperty(password));
         ReusableMethods.wait(2);
-        aileButcemPage.girisYapButon.click();
+        ReusableMethods.scrollWithUiScrollable(girisYap); // feature'daki "Giriş Yap" string değerinin direk almasını istediğimiz için parametredeki değeri bu satıra yazıyoruz.
     }
 
     @Given("uygulamaya kullanici bilgileriyle giris yapildigini dogrulayin")
     public void uygulamaya_kullanici_bilgileriyle_giris_yapildigini_dogrulayin() {
-
-
+        Assert.assertTrue(aileButcemPage.girisYapildiText.isDisplayed());
     }
 
-
-    @Given("sol kisimdaki menuden hesabim bolumune gidin")
-    public void sol_kisimdaki_menuden_hesabim_bolumune_gidin() {
-
+    @Given("sol kisimdaki menuden {string} bolumune gidin")
+    public void sol_kisimdaki_menuden_hesabim_bolumune_gidin(String hesabim) throws InterruptedException {
+        aileButcemPage.ucCizgiTiklamaMethodu();
+        ReusableMethods.scrollWithUiScrollable(hesabim);
     }
 
+    @Given("hesabim sayfasindaki bilgileri degistirerek {string} {string} {string} {string} {string} degisikleri kaydedin ve dogrulayin")
+    public void hesabim_sayfasindaki_bilgileri_degistirerek_degisikleri_kaydedin(String isim,String soyisim,String sehir,String yas,String meslek) {
 
-    @Given("hesabim sayfasindaki bilgileri degistirerek degisikleri kaydedin")
-    public void hesabim_sayfasindaki_bilgileri_degistirerek_degisikleri_kaydedin() {
-
+        aileButcemPage.textBoxVeriGirme(isim, soyisim,sehir,yas,meslek);
+        aileButcemPage.textBoxKontrol(isim, soyisim,sehir,yas,meslek);
     }
 
-
-    @Given("ardindan degisiklerin yapildigini dogrulayin")
-    public void ardindan_degisiklerin_yapildigini_dogrulayin() {
-
-    }
 
 
 }
